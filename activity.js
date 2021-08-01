a4f3fa09d4aad357852d887c16ccd781238e67fe
Karl-Alexander Hoppe <hoppe397@gmail.com>
2021-08-01 17:03:37 +0200

    multiple timers, cannot run at the same time

--- activity.js
+++ activity.js
@@ -7,10 +7,14 @@ class Task {
     constructor(name, duration) {
         this.name = name;
         this.duration = duration;
+        this.active = false;
         this.counting = false;
         this.resumeTime = 0;
         this.cumulativeTime = 0;
-        this.outputId = "output";
+        this.outputId = this.name + "Output";
+    }
+    start(inst) {
+        displayTime(inst);
     }
     countUp(refreshRate) {
         this.cumulativeTime += refreshRate;
@@ -18,16 +22,14 @@ class Task {
     }
 }
 
-let tt = new Task("paper", 120);
-
 //app object for initialization
 // do this individually for each button? (replacing "tt" with "this")
-const app = {
-    running : false,
-    start: function(taskInstance = null) {
-        displayTime(taskInstance);
-    }
-};
+// const app = {
+//     running : false,
+//     start: function(taskInstance = null) {
+//         displayTime(taskInstance);
+//     }
+// };
 
 function displayTime(inst=null) {
     if (inst.counting === true) {
@@ -46,28 +48,36 @@ for (var i = 0; i < buttons.length; i++){
     buttons[i].addEventListener('click',redirect);
 }
 
+// empty object to keep track of instantiated Tasks
+tasks = {}
+
 function redirect(ev){
     console.log("you clicked on "+ev.target.id);
-    // alert(task.target.id);
     //--------------------------------------------------------------------------
-    // does a button instance for the clicked on innerHTML already exist?
+    // does a button instance for the clicked on id already exist?
     // make a dictionary and see if the respective key exists
     // if the key does not exist, instantiate a new button
-    // check, whether the app is running
-    if (app.running === false) {
-        app.start(tt);
-        app.running = true;
+    if (!(ev.target.id in tasks)) {
+        tasks[ev.target.id] = new Task(ev.target.id, 120);
     }
+
+    // check, whether the task is active
+    if (tasks[ev.target.id].active === false) {
+        tasks[ev.target.id].start(tasks[ev.target.id]);
+        tasks[ev.target.id].active = true;
+    }
+
     // start counting
-    if (tt.counting === false) {
-        tt.resumeTime = Math.floor(Date.now() / 1000);
-        tt.counting = true;
+    if (tasks[ev.target.id].counting === false) {
+        tasks[ev.target.id].resumeTime = Math.floor(Date.now() / 1000);
+        tasks[ev.target.id].counting = true;
         console.log("started counting")
         return;
     }
+
     // stop counting
-    if (tt.counting === true) {
-        tt.counting = false;
+    if (tasks[ev.target.id].counting === true) {
+        tasks[ev.target.id].counting = false;
         console.log("stopped counting")
         return;
     }
